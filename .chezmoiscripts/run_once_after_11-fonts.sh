6b69b3ff96a493532371eb5467efb6cefbfd2c1d
noeltz <161815407+noeltz@users.noreply.github.com>
2026-03-20 12:57:16 +0100

    Update and rename run_once_after_11-tabler-webfont.sh to run_once_after_11-fonts.sh

---
+++ .chezmoiscripts/run_once_after_11-fonts.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+# --- CONFIGURATION ---
+FONT_DIR="$HOME/.local/share/fonts"
+ZIP_URLS=(
+    https://github.com/noeltz/custom-maple-font/releases/latest/download/NFM_MapleMonoNormal-NF.zip
+    https://github.com/noeltz/custom-maple-font/releases/latest/download/NFP_MapleMonoNormal-NF.zip
+)
+GITHUB_REPOS=(
+    #noeltz/custom-maple-font
+)
+# ---------------------
+
+mkdir -p "$FONT_DIR"
+TEMP_DIR=$(mktemp -d)
+trap 'rm -rf "$TEMP_DIR"' EXIT
+cd "$TEMP_DIR"
+
+ALL_ZIPS=("${ZIP_URLS[@]}")
+
+for repo in "${GITHUB_REPOS[@]}"; do
+    while IFS= read -r url; do
+        [[ -n "$url" ]] && ALL_ZIPS+=("$url")
+    done < <(curl -s "https://api.github.com/repos/${repo}/releases/latest" | jq -r '.assets[] | select(.name | endswith(".zip")) | .browser_download_url')
+done
+
+for url in "${ALL_ZIPS[@]}"; do
+    [[ -z "$url" ]] && continue
+    curl -L -s -O "$url"
+    zip_file=$(basename "$url")
+    unzip -q -o "$zip_file"
+    find . -type f -iname "*.ttf" -exec cp {} "$FONT_DIR" \;
+done
+
+fc-cache -f
