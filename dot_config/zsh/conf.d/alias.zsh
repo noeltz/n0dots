f9068b4659bc2f70f12862918e794f4338539b2e
noeltz <161815407+noeltz@users.noreply.github.com>
2026-03-21 23:03:26 +0100

    Update alias.zsh

--- dot_config/zsh/conf.d/alias.zsh
+++ dot_config/zsh/conf.d/alias.zsh
@@ -1,17 +1,55 @@
 #!/usr/bin/env zsh
 
-alias c='clear' \
-    ..='cd ..' \
-    ...='cd ../..' \
-    .3='cd ../../..' \
-    .4='cd ../../../..' \
-    .5='cd ../../../../..' \
-    mkdir='mkdir -p'
-alias ff='fastfetch'
+#######################################
+# ALIASES
+#######################################
+
+# File and Directory Operations
+alias y='yazi'
+alias ls='eza --icons --grid --group-directories-first'
+alias ll='eza -lah --icons --group-directories-first'
+alias lt='eza --tree --icons --group-directories-first'
+alias la='eza -a --icons --grid --group-directories-first'
+alias cat='bat -pp'
+alias mkdir='mkdir -p'
+alias cd='z'
+alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
 alias lg='lazygit'
 export EDITOR='nvim'
 export BAT_THEME='ansi'
 
+# System Utilities
+alias c='clear'
+alias e='exit'
+alias f='fastfetch'
+alias help='tldr'
+alias history='history 1'
+alias paste="wl-paste"
+
+copy() {
+  if [[ $# -eq 1 ]]; then
+    wl-copy < "$1"
+  elif [[ $# -gt 1 ]]; then
+    command cat -- "$@" | wl-copy
+  else
+    wl-copy
+  fi
+}
+
+# Development Tools
+alias nvimconfig="cd ~/.config/nvim && nvim ."
+alias n="nvim"
+alias hx='helix'
+alias ld='lazydocker'
+# alias zed='zeditor'
+alias code='vscodium'
+
+# Search and Find
+alias fman='compgen -c | fzf | xargs man'
+alias fzf-find='fd --type f | fzf'
+alias find='fd'
+
+# Package Helpers
 if [[ ! "$(which yay)" && "$(which paru)" ]]; then
   alias yay='paru'
 elif [[ ! "$(which paru)" && "$(which yay)" ]]; then
