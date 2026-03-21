2a13a02a743703acb224141078824fd39904d129
noeltz <161815407+noeltz@users.noreply.github.com>
2026-03-21 22:59:03 +0100

    Update sheldon.zsh

--- dot_config/zsh/plugins/sheldon.zsh
+++ dot_config/zsh/plugins/sheldon.zsh
@@ -1,1 +1,7 @@
+#######################################
+# SHELDON (PLUGIN MANAGER)
+#######################################
+export ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/oh-my-zsh"
+mkdir -p "$ZSH_CACHE_DIR/completions"
 
+eval "$(sheldon source)"
