49bc5723ddb50f50dd2afac3c339a0f26a2ffbce
noeltz <161815407+noeltz@users.noreply.github.com>
2026-03-21 22:52:44 +0100

    Delete dot_config/zsh/fzf.zsh

--- dot_config/zsh/fzf.zsh
+++
@@ -1,9 +0,0 @@
-#######################################
-# FZF CONFIG
-#######################################
-source <(fzf --zsh)
-
-export FZF_CTRL_R_OPTS="
-  --bind 'ctrl-y:execute-silent(echo -n {2..} | wl-copy)+abort'
-  --color header:italic
-  --header 'Press CTRL-Y to copy command into clipboard'"
