02c8665adff4fa1819e06643a348e7b1f8d67d65
noeltz <161815407+noeltz@users.noreply.github.com>
2026-03-23 08:47:33 +0100

    Update sheldon.zsh

--- dot_config/zsh/plugins/sheldon.zsh
+++ dot_config/zsh/plugins/sheldon.zsh
@@ -4,4 +4,9 @@
 export ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/oh-my-zsh"
 mkdir -p "$ZSH_CACHE_DIR/completions"
 
+autoload -Uz compinit
+compinito
+
 eval "$(sheldon source)"
+# Plugin-specific settings
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
